c1cf3c883af4391103313e646520833b1343534e
Bjarne Grimstad <bjarne.grimstad@gmail.com>
2024-03-13 17:30:42 +0100

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# logistic-de
-Solving the logistic differential equation numerically using backward Euler, implemented in Casadi
+## Simulation of the logistic differential equation
+
+In this project, you will find an example on how to solve the logistic differential equation numerically using the 
+backward (implicit) Euler method. The method results in a nonlinear optimization problem, which is solved using Ipopt 
+(via Casadi). 
+
+### The logistic differential equation
+The logistic differential equation is given by:
+d/dz f(z) = f(z) (1 - f(z)),
+where z is a real number.
+
+This equation has an analytical solution of the form: 
+f(z) = exp(z) / (exp(z) + C).
+
+The boundary condition f(0) = 1/2 yields the integration constant C = 1. 
+The solution can then be written as f(z) = 1 / (1 + exp(-z)), which is known as the logistic curve.
+
+### Numerical integration
+We will solve the equation for z in [0, L], given the boundary condition f(0) = 1/2.
+
+Numerical integration is performed using the backward/implicit Euler method. The solution method has the following steps: 
+1. Discretize [0, L] into n intervals of length dz = L/n. Denote z[i] = i * dz and x[i] = f(z[i]) for i=0,...,n. 
+2. Create Casadi variables: x[i] for i=0,...,n. 
+3. Create Casadi expressions for the boundary condition, g[0] = x[0] - 1/2, and discretized differential equation: g[i] = x[i] - x[i-1] - dz * x[i] * (1 - x[i]) for i=1,...,n.
+4. Solve the following feasibility problem using Ipopt: minimize 0 subject to g[i] = 0 for i=0,...,n, where the optimization variables are x[0],...,x[n].
+5. Return the solution x[0],...,x[n]
---
+++ main.py
@@ -0,0 +1,92 @@
+"""
+Created 13 March 2024
+Bjarne Grimstad, bjarne.grimstad@gmail.no
+"""
+
+import casadi as ca
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def logistic_curve(u):
+    return 1 / (1 + np.exp(-u))
+
+
+def solve_numerically(L, x0=1 / 2, n=100):
+    """
+    Solve the logistic differential equation numerically using the backward Euler method
+    This leads to a system of nonlinear equations, which we can cast as a non-linear programming problem
+    The resulting NLP problem is solved using Ipopt (via Casadi)
+
+    :return: Solution
+    """
+
+    # We divide u into n intervals of length du
+    du = L / n
+
+    # Create lists to hold variables and equations/constraints
+    x = list()  # Variables
+    g = list()  # Constraints (system of equations)
+
+    # Add variables and equations for all intervals
+    for i in range(n + 1):  # Note that we add one to get a total length of L
+
+        # Variable for interval i: x[i] = f(u[i])
+        x.append(ca.SX.sym(f'x_{i}'))
+
+        if i == 0:
+            # Add left boundary condition as equality constraint: x[0] - x0 = 0 => x[0] = u0
+            g.append(x[i] - x0)
+
+        else:  # Cells 1,...,n
+            # Discretized differential equations as equality constraint (using backward Euler scheme)
+            de = x[i] - x[i-1] - du * x[i] * (1 - x[i])
+            g.append(de)
+
+    # Create variable and constraint vectors (for Casadi)
+    x_vec = ca.vertcat(*x)
+    g_vec = ca.vertcat(*g)
+
+    # Initial guess on solution
+    x_guess = [x0] * len(x)
+
+    # Constraint bounds
+    # Equality constraints, g(x) = 0, are implemented as: 0 <= g(x) <= 0
+    lbg = [0] * len(g)
+    ubg = [0] * len(g)
+
+    # Solve system of equations using Ipopt
+    f = 0  # We set the objective function, f, to zero to solve a feasibility problem
+    nlp = {'x': x_vec, 'f': f, 'g': g_vec}
+    solver_config = {'ipopt.print_level': 0, 'print_time': 0}
+    solver = ca.nlpsol('S', 'ipopt', nlp, solver_config)
+    result = solver(x0=x_guess, lbg=lbg, ubg=ubg)
+    if not solver.stats()['success']:
+        print('Solver status:', solver.stats())
+        raise Exception('Solve was not successful')
+
+    x_opt = result['x']
+    x_opt = x_opt.full().flatten().tolist()  # Convert solution to flat numpy array and then to a list
+
+    return x_opt
+
+
+if __name__ == '__main__':
+    L = 4       # u is in [0, L]
+    x0 = 1/2    # Boundary condition f(0) = x0
+    n = 100     # Number of cells in discretization
+
+    # Solve numerically
+    x_numerical = solve_numerically(L, x0, n)
+
+    # Solve analytically
+    u = np.linspace(0, L, n+1)
+    x_analytical = logistic_curve(u)
+
+    # Plot results
+    plt.figure()
+    plt.plot(u, x_numerical, label='Numerical')
+    plt.plot(u, x_analytical, '--r', label='Analytical')
+    plt.legend()
+    plt.show()
+
